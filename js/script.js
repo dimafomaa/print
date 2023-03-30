1eca8171d5bc7c5f86c562a98fbe596eed6e18ce
Dima <103756308+dimafomaa@users.noreply.github.com>
2023-03-30 18:00:08 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-$(document).ready(function(){
-    $('.header__burger').click(function(event){
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
         $('.header__burger, .menu, .header__btn').toggleClass('active');
         $('body').toggleClass('lock');
         $('.menu').toggleClass('open');
     });
-    $('.menu__link').click(function(event){
+    $('.menu__link').click(function (event) {
         $('.header__burger, .menu, .header__btn').removeClass('active');
         $('body').removeClass('lock');
     });
+
+    $(".gallery__wrapper").magnificPopup({
+        delegate: "a",
+        type: "image",
+        gallery: {
+            enabled: true
+        }
+    });
+
+});
+
+const swiper = new Swiper('.gallery__swiper', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    slidesPerColumn: 3,   
 });
