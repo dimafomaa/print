22a7a73b3fb99114cf19168ae5426624f2f692b4
Dima <103756308+dimafomaa@users.noreply.github.com>
2023-04-08 22:30:44 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,188 +1,239 @@
-$(document).ready(function () {
+"use strict"
 
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('form');
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		e.preventDefault();
+
+		let error = formValidate(form);
+
+		let formData = new FormData(form);
+		formData.append('image', formImage.files[0]);
+
+		if (error === 0) {
+			let response = await fetch('send-mail.php', {
+				method: 'POST',
+				body: formData
+			});
+			if (response.ok) {
+				window.location.href = '/thank.html';
+			} else {
+				alert("Ошибка");
+			}
+		} else {
+			alert('Заполните обязательные поля');
+		}
+
+	}
+
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req');
+
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input);
+
+			if (input.classList.contains('_email')) {
+				if (emailTest(input)) {
+					formAddError(input);
+					error++;
+				}
+			} else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+				formAddError(input);
+				error++;
+			} else {
+				if (input.value === '') {
+					formAddError(input);
+					error++;
+				}
+			}
+		}
+		return error;
+	}
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+	//Функция теста email
+	function emailTest(input) {
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	}
+
+	//Получаем инпут file в переменную
+	const formImage = document.getElementById('formImage');
+	//Получаем див для превью в переменную
+	const formPreview = document.getElementById('formPreview');
+
+	//Слушаем изменения в инпуте file
+	formImage.addEventListener('change', () => {
+		uploadFile(formImage.files[0]);
+	});
+
+	function uploadFile(file) {
+		// провераяем тип файла
+		if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+			alert('Разрешены только изображения.');
+			formImage.value = '';
+			return;
+		}
+		// проверим размер файла (<20 Мб)
+		if (file.size > 20 * 4500 * 4500) {
+			alert('Файл должен быть менее 20 МБ.');
+			return;
+		}
+
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+		};
+		reader.onerror = function (e) {
+			alert('Ошибка');
+		};
+		reader.readAsDataURL(file);
+	}
+});
+
+
+
+
+
+$(document).ready(function () {
 
     $('input[type="tel"]').inputmask("+38 (999) 999-99-99");
 
-    $('form').on('submit', function (e) {
-		e.preventDefault(); // предотвращение стандартного поведения формы
-		var form = $(this);
-
-		// отправка формы с помощью AJAX
-		$.ajax({
-			url: 'send-mail.php',
-			type: 'POST',
-			data: form.serialize(),
-			success: function (data) {
-				form[0].reset(); // очистка формы
-				// Открываем модальное окно благодарности после успешной отправки формы
-				const thankPopup = document.getElementById('modal-thank');
-				popupOpen(thankPopup);
-			}
-		});
-	});
-
-    // закрытие модального окна при нажатии на кнопку закрытия
-
-
-    //Получаем инпут file в переменную
-    const formImage = document.getElementById('formImage');
-    //Получаем див для превью в переменную
-    const formPreview = document.getElementById('formPreview');
-
-    //Слушаем изменения в инпуте file
-    formImage.addEventListener('change', () => {
-        uploadFile(formImage.files[0]);
-    });
-
-    function uploadFile(file) {
-        // провераяем тип файла
-        if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
-            alert('Разрешены только изображения.');
-            formImage.value = '';
-            return;
-        }
-        // проверим размер файла (<2 Мб)
-        if (file.size > 20 * 1024 * 1024) {
-            alert('Файл должен быть менее 50 МБ.');
-            return;
-        }
-
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
-        };
-        reader.onerror = function (e) {
-            alert('Ошибка');
-        };
-        reader.readAsDataURL(file);
-    }
-
-
 });
 
-const popupLinks = document.querySelectorAll('.popup-link');
-const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll(".lock-padding");
+// const popupLinks = document.querySelectorAll('.popup-link');
+// const body = document.querySelector('body');
+// const lockPadding = document.querySelectorAll(".lock-padding");
 
-let unlock = true;
+// let unlock = true;
 
-const timeout = 300;
+// const timeout = 300;
 
-if (popupLinks.length > 0) {
-	for (let index = 0; index < popupLinks.length; index++) {
-		const popupLink = popupLinks[index];
-		popupLink.addEventListener("click", function (e) {
-			const popupName = popupLink.getAttribute('href').replace('#', '');
-			const curentPopup = document.getElementById(popupName);
-			popupOpen(curentPopup);
-			e.preventDefault();
-		});
-	}
-}
-const popupCloseIcon = document.querySelectorAll('.close-popup');
-if (popupCloseIcon.length > 0) {
-	for (let index = 0; index < popupCloseIcon.length; index++) {
-		const el = popupCloseIcon[index];
-		el.addEventListener('click', function (e) {
-			popupClose(el.closest('.popup'));
-			e.preventDefault();
-		});
-	}
-}
+// if (popupLinks.length > 0) {
+// 	for (let index = 0; index < popupLinks.length; index++) {
+// 		const popupLink = popupLinks[index];
+// 		popupLink.addEventListener("click", function (e) {
+// 			const popupName = popupLink.getAttribute('href').replace('#', '');
+// 			const curentPopup = document.getElementById(popupName);
+// 			popupOpen(curentPopup);
+// 			e.preventDefault();
+// 		});
+// 	}
+// }
+// const popupCloseIcon = document.querySelectorAll('.close-popup');
+// if (popupCloseIcon.length > 0) {
+// 	for (let index = 0; index < popupCloseIcon.length; index++) {
+// 		const el = popupCloseIcon[index];
+// 		el.addEventListener('click', function (e) {
+// 			popupClose(el.closest('.popup'));
+// 			e.preventDefault();
+// 		});
+// 	}
+// }
 
-function popupOpen(curentPopup) {
-	if (curentPopup && unlock) {
-		const popupActive = document.querySelector('.popup.open');
-		if (popupActive) {
-			popupClose(popupActive, false);
-		} else {
-			bodyLock();
-		}
-		curentPopup.classList.add('open');
-		curentPopup.addEventListener("click", function (e) {
-			if (!e.target.closest('.popup__content')) {
-				popupClose(e.target.closest('.popup'));
-			}
-		});
-	}
-}
+// function popupOpen(curentPopup) {
+// 	if (curentPopup && unlock) {
+// 		const popupActive = document.querySelector('.popup.open');
+// 		if (popupActive) {
+// 			popupClose(popupActive, false);
+// 		} else {
+// 			bodyLock();
+// 		}
+// 		curentPopup.classList.add('open');
+// 		curentPopup.addEventListener("click", function (e) {
+// 			if (!e.target.closest('.popup__content')) {
+// 				popupClose(e.target.closest('.popup'));
+// 			}
+// 		});
+// 	}
+// }
 
-function popupClose(popupActive, doUnlock = true) {
-	if (unlock) {
-		popupActive.classList.remove('open');
-		if (doUnlock) {
-			bodyUnLock();
-		}
-	}
-}
+// function popupClose(popupActive, doUnlock = true) {
+// 	if (unlock) {
+// 		popupActive.classList.remove('open');
+// 		if (doUnlock) {
+// 			bodyUnLock();
+// 		}
+// 	}
+// }
 
-function bodyLock() {
-	const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+// function bodyLock() {
+// 	const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
 
-	if (lockPadding.length > 0) {
-		for (let index = 0; index < lockPadding.length; index++) {
-			const el = lockPadding[index];
-			el.style.paddingRight = lockPaddingValue;
-		}
-	}
-	body.style.paddingRight = lockPaddingValue;
-	body.classList.add('lock');
+// 	if (lockPadding.length > 0) {
+// 		for (let index = 0; index < lockPadding.length; index++) {
+// 			const el = lockPadding[index];
+// 			el.style.paddingRight = lockPaddingValue;
+// 		}
+// 	}
+// 	body.style.paddingRight = lockPaddingValue;
+// 	body.classList.add('lock');
 
-	unlock = false;
-	setTimeout(function () {
-		unlock = true;
-	}, timeout);
-}
+// 	unlock = false;
+// 	setTimeout(function () {
+// 		unlock = true;
+// 	}, timeout);
+// }
 
-function bodyUnLock() {
-	setTimeout(function () {
-		if (lockPadding.length > 0) {
-			for (let index = 0; index < lockPadding.length; index++) {
-				const el = lockPadding[index];
-				el.style.paddingRight = '0px';
-			}
-		}
-		body.style.paddingRight = '0px';
-		body.classList.remove('lock');
-	}, timeout);
+// function bodyUnLock() {
+// 	setTimeout(function () {
+// 		if (lockPadding.length > 0) {
+// 			for (let index = 0; index < lockPadding.length; index++) {
+// 				const el = lockPadding[index];
+// 				el.style.paddingRight = '0px';
+// 			}
+// 		}
+// 		body.style.paddingRight = '0px';
+// 		body.classList.remove('lock');
+// 	}, timeout);
 
-	unlock = false;
-	setTimeout(function () {
-		unlock = true;
-	}, timeout);
-}
+// 	unlock = false;
+// 	setTimeout(function () {
+// 		unlock = true;
+// 	}, timeout);
+// }
 
-document.addEventListener('keydown', function (e) {
-	if (e.which === 27) {
-		const popupActive = document.querySelector('.popup.open');
-		popupClose(popupActive);
-	}
-});
+// document.addEventListener('keydown', function (e) {
+// 	if (e.which === 27) {
+// 		const popupActive = document.querySelector('.popup.open');
+// 		popupClose(popupActive);
+// 	}
+// });
 
-(function () {
-	// проверяем поддержку
-	if (!Element.prototype.closest) {
-		// реализуем
-		Element.prototype.closest = function (css) {
-			var node = this;
-			while (node) {
-				if (node.matches(css)) return node;
-				else node = node.parentElement;
-			}
-			return null;
-		};
-	}
-})();
-(function () {
-	// проверяем поддержку
-	if (!Element.prototype.matches) {
-		// определяем свойство
-		Element.prototype.matches = Element.prototype.matchesSelector ||
-			Element.prototype.webkitMatchesSelector ||
-			Element.prototype.mozMatchesSelector ||
-			Element.prototype.msMatchesSelector;
-	}
-})();
+// (function () {
+// 	// проверяем поддержку
+// 	if (!Element.prototype.closest) {
+// 		// реализуем
+// 		Element.prototype.closest = function (css) {
+// 			var node = this;
+// 			while (node) {
+// 				if (node.matches(css)) return node;
+// 				else node = node.parentElement;
+// 			}
+// 			return null;
+// 		};
+// 	}
+// })();
+// (function () {
+// 	// проверяем поддержку
+// 	if (!Element.prototype.matches) {
+// 		// определяем свойство
+// 		Element.prototype.matches = Element.prototype.matchesSelector ||
+// 			Element.prototype.webkitMatchesSelector ||
+// 			Element.prototype.mozMatchesSelector ||
+// 			Element.prototype.msMatchesSelector;
+// 	}
+// })();
 
 
 $(document).ready(function () {
