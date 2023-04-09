47585337e20c668db758541832a703f9cc6646dd
Dima <103756308+dimafomaa@users.noreply.github.com>
2023-04-09 10:44:02 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,6 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
 		let formData = new FormData(form);
 		formData.append('image', formImage.files[0]);
 
+		let loadingIcon = document.querySelector('.loading-icon');
+		loadingIcon.classList.add('active'); // show loading icon
+
 		if (error === 0) {
 			let response = await fetch('send-mail.php', {
 				method: 'POST',
@@ -26,7 +29,9 @@ document.addEventListener('DOMContentLoaded', function () {
 			alert('Заполните обязательные поля');
 		}
 
+		loadingIcon.classList.remove('active'); // hide loading icon
 	}
+
 
 
 	function formValidate(form) {
@@ -107,7 +112,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 $(document).ready(function () {
 
-    $('input[type="tel"]').inputmask("+38 (999) 999-99-99");
+	$('input[type="tel"]').inputmask("+38 (999) 999-99-99");
 
 });
 
@@ -237,84 +242,84 @@ $(document).ready(function () {
 
 
 $(document).ready(function () {
-    $('.header__burger').click(function (event) {
-        $('.header__burger, .menu, .header__btn').toggleClass('active');
-        $('body').toggleClass('lock');
-        $('.menu').toggleClass('open');
-    });
-    $('.menu__link').click(function (event) {
-        $('.header__burger, .menu, .header__btn').removeClass('active');
-        $('body').removeClass('lock');
-    });
+	$('.header__burger').click(function (event) {
+		$('.header__burger, .menu, .header__btn').toggleClass('active');
+		$('body').toggleClass('lock');
+		$('.menu').toggleClass('open');
+	});
+	$('.menu__link').click(function (event) {
+		$('.header__burger, .menu, .header__btn').removeClass('active');
+		$('body').removeClass('lock');
+	});
 
 
-    $(document).ready(function () {
-        $('.gallery__wrapper, .reviews__wrapper').magnificPopup({
-            delegate: 'a',
-            type: 'image',
-            closeOnContentClick: false,
-            closeBtnInside: false,
-            mainClass: 'mfp-with-zoom mfp-img-mobile',
+	$(document).ready(function () {
+		$('.gallery__wrapper, .reviews__wrapper').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			closeOnContentClick: false,
+			closeBtnInside: false,
+			mainClass: 'mfp-with-zoom mfp-img-mobile',
 
-            gallery: {
-                enabled: true
-            },
-            zoom: {
-                enabled: true,
-                duration: 300,
-                opener: function (element) {
-                    return element.find('img');
-                }
-            }
+			gallery: {
+				enabled: true
+			},
+			zoom: {
+				enabled: true,
+				duration: 300,
+				opener: function (element) {
+					return element.find('img');
+				}
+			}
 
-        });
-    });
+		});
+	});
 
-    $('.question__block--title').click(function (event) {
-        if ($('.question__block').hasClass('one')) {
-            $('.question__block--title').not($(this)).removeClass('active');
-            $('.question__block--text').not($(this).next()).slideUp(300);
+	$('.question__block--title').click(function (event) {
+		if ($('.question__block').hasClass('one')) {
+			$('.question__block--title').not($(this)).removeClass('active');
+			$('.question__block--text').not($(this).next()).slideUp(300);
 
-        }
-        $(this).toggleClass('active').next().slideToggle(300);
+		}
+		$(this).toggleClass('active').next().slideToggle(300);
 
-    });
+	});
 
 });
 
 const swiper = new Swiper('.reviews__swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    slidesPerGroup: 1,
-    slidesPerView: 3,
-    spaceBetween: 30,
-    autoHeight: true,
-    loop: true,
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-            
-        },
-        600: {
-            slidesPerView: 2,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	slidesPerGroup: 1,
+	slidesPerView: 3,
+	spaceBetween: 30,
+	autoHeight: true,
+	loop: true,
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
 
-        },
-        900: {
-            slidesPerView: 3,
-        },
-    }
+		},
+		600: {
+			slidesPerView: 2,
+
+		},
+		900: {
+			slidesPerView: 3,
+		},
+	}
 });
 
 const scrollUp = () => {
-    const scrollUp = document.getElementById('scroll-up')
-    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-        : scrollUp.classList.remove('show-scroll')
+	const scrollUp = document.getElementById('scroll-up')
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+		: scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
@@ -332,20 +337,20 @@ const priceElement = document.getElementById('price');
 
 // Функция для обновления цены
 function updatePrice() {
-    // Получаем значения выбранных элементов формы
-    const sizeValue = sizeSelect.options[sizeSelect.selectedIndex].value;
-    const sizePrice = parseInt(sizeSelect.options[sizeSelect.selectedIndex].dataset.price);
-    const stylePrice = parseInt(styleSelect.options[styleSelect.selectedIndex].dataset.price || '0');
-    const stretchPrice = parseInt(stretchSelect.options[stretchSelect.selectedIndex].dataset.price || '0');
-    const materialPrice = parseInt(materialSelect.options[materialSelect.selectedIndex].dataset.price || '0');
-    const packagingPrice = packagingCheckbox.checked ? 70 : 0;
-    const urgentPrice = urgentCheckbox.checked ? 100 : 0;
+	// Получаем значения выбранных элементов формы
+	const sizeValue = sizeSelect.options[sizeSelect.selectedIndex].value;
+	const sizePrice = parseInt(sizeSelect.options[sizeSelect.selectedIndex].dataset.price);
+	const stylePrice = parseInt(styleSelect.options[styleSelect.selectedIndex].dataset.price || '0');
+	const stretchPrice = parseInt(stretchSelect.options[stretchSelect.selectedIndex].dataset.price || '0');
+	const materialPrice = parseInt(materialSelect.options[materialSelect.selectedIndex].dataset.price || '0');
+	const packagingPrice = packagingCheckbox.checked ? 70 : 0;
+	const urgentPrice = urgentCheckbox.checked ? 100 : 0;
 
-    // Считаем итоговую цену
-    const totalPrice = sizePrice + stylePrice + stretchPrice + materialPrice + packagingPrice + urgentPrice;
+	// Считаем итоговую цену
+	const totalPrice = sizePrice + stylePrice + stretchPrice + materialPrice + packagingPrice + urgentPrice;
 
-    // Обновляем текст элемента с ценой
-    priceElement.innerText = `${totalPrice} грн`;
+	// Обновляем текст элемента с ценой
+	priceElement.innerText = `${totalPrice} грн`;
 }
 
 // Вызываем функцию для первоначальной установки цены
